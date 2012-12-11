131d551e9fd15dd7d54b32c52b1f974f6bde7a53
kossboss <git@kossboss.com>
2012-12-11 00:09:36 -0800

    Starting the Session Mapper.
    Still in Development
    Got TCP IP settings
    Got Location Tracking had to get API key
    Still need to put on google map
    Still need to thread
    Still need to fine tune
    Still need to make look awesome
    Still need to make work with timer or something

---
+++ SessionMapper/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace SessionMapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bstart = new System.Windows.Forms.Button();
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.rtbTl = new System.Windows.Forms.RichTextBox();
+            this.rtbUl = new System.Windows.Forms.RichTextBox();
+            this.lstats = new System.Windows.Forms.Label();
+            this.bb = new System.Windows.Forms.Button();
+            this.rrtb = new System.Windows.Forms.RichTextBox();
+            this.tip = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bstart
+            // 
+            this.bstart.Location = new System.Drawing.Point(13, 13);
+            this.bstart.Name = "bstart";
+            this.bstart.Size = new System.Drawing.Size(202, 23);
+            this.bstart.TabIndex = 0;
+            this.bstart.Text = "Start";
+            this.bstart.UseVisualStyleBackColor = true;
+            this.bstart.Click += new System.EventHandler(this.bstart_Click);
+            // 
+            // rtb
+            // 
+            this.rtb.Location = new System.Drawing.Point(12, 42);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(448, 607);
+            this.rtb.TabIndex = 1;
+            // 
+            // rtbTl
+            // 
+            this.rtbTl.Location = new System.Drawing.Point(466, 42);
+            this.rtbTl.Name = "rtbTl";
+            this.rtbTl.Size = new System.Drawing.Size(202, 607);
+            this.rtbTl.TabIndex = 1;
+            // 
+            // rtbUl
+            // 
+            this.rtbUl.Location = new System.Drawing.Point(674, 42);
+            this.rtbUl.Name = "rtbUl";
+            this.rtbUl.Size = new System.Drawing.Size(202, 607);
+            this.rtbUl.TabIndex = 1;
+            // 
+            // lstats
+            // 
+            this.lstats.AutoSize = true;
+            this.lstats.Location = new System.Drawing.Point(231, 13);
+            this.lstats.Name = "lstats";
+            this.lstats.Size = new System.Drawing.Size(34, 13);
+            this.lstats.TabIndex = 2;
+            this.lstats.Text = "Stats:";
+            // 
+            // bb
+            // 
+            this.bb.Location = new System.Drawing.Point(944, 42);
+            this.bb.Name = "bb";
+            this.bb.Size = new System.Drawing.Size(75, 57);
+            this.bb.TabIndex = 3;
+            this.bb.Text = "Get Location";
+            this.bb.UseVisualStyleBackColor = true;
+            this.bb.Click += new System.EventHandler(this.bb_Click);
+            // 
+            // rrtb
+            // 
+            this.rrtb.Location = new System.Drawing.Point(907, 189);
+            this.rrtb.Name = "rrtb";
+            this.rrtb.Size = new System.Drawing.Size(140, 170);
+            this.rrtb.TabIndex = 4;
+            // 
+            // tip
+            // 
+            this.tip.Location = new System.Drawing.Point(930, 138);
+            this.tip.Name = "tip";
+            this.tip.Size = new System.Drawing.Size(100, 20);
+            this.tip.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 661);
+            this.Controls.Add(this.tip);
+            this.Controls.Add(this.rrtb);
+            this.Controls.Add(this.bb);
+            this.Controls.Add(this.lstats);
+            this.Controls.Add(this.rtbUl);
+            this.Controls.Add(this.rtbTl);
+            this.Controls.Add(this.rtb);
+            this.Controls.Add(this.bstart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bstart;
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.RichTextBox rtbTl;
+        private System.Windows.Forms.RichTextBox rtbUl;
+        private System.Windows.Forms.Label lstats;
+        private System.Windows.Forms.Button bb;
+        private System.Windows.Forms.RichTextBox rrtb;
+        private System.Windows.Forms.TextBox tip;
+    }
+}
+
---
+++ SessionMapper/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+
+
+
+namespace SessionMapper
+{
+    public partial class Form1 : Form
+    {
+
+        const string Version = "1.0";
+        const string Title = "Session Map v" + Version;
+        string nl = Environment.NewLine;
+        bool running = false;
+        const string StopMessage = "Stop Displaying Sessions";
+        const string StartMessage = "Start Displaying Sessions";
+        ArrayList mappoints;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bstart.Text = StartMessage;
+            this.Text = Title;
+        }
+
+        private void bstart_Click(object sender, EventArgs e)
+        {
+            if (running == false)
+            {
+                running = true;
+                bstart.Text = StopMessage;
+                Record();
+            }
+            else
+            {
+                running = false;
+                bstart.Text = StartMessage;
+            }
+
+        }
+        private void ClearAll()
+        {
+
+            rtb.Clear();
+            rtbUl.Clear();
+            rtbTl.Clear();
+        }
+        private void InvalidateAll()
+        {
+            rtb.Invalidate();
+            rtbUl.Invalidate();
+            rtbTl.Invalidate();
+        }
+        private void Record()
+        {
+            ClearAll();
+            IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] endPointsTcp = ipProperties.GetActiveTcpListeners();
+            IPEndPoint[] endPointsUdp = ipProperties.GetActiveUdpListeners();
+            TcpConnectionInformation[] tcpConnections = ipProperties.GetActiveTcpConnections();
+            UdpStatistics Ustats = ipProperties.GetUdpIPv4Statistics();
+            TcpStatistics Tstats = ipProperties.GetTcpIPv4Statistics();
+            //// ACTIVE CONNECTIONS
+            //// ACTIVE CONNECTIONS
+            //// ACTIVE CONNECTIONS
+            rtb.AppendText("== Active Connections ==" + nl);
+            string remoteip = "";
+            string localip = "";
+            string localport = "";
+            string remoteport = "";
+            string TcpState1 = "";
+            location loc = new location();
+            int tcplist = 0;
+            int udplist = 0;
+            int tcpconnlist = 0;
+            int maplist=0;
+            Boolean mapable = false;
+            point pt;
+            foreach (TcpConnectionInformation info in tcpConnections)
+            {
+                localip = info.LocalEndPoint.Address.ToString();
+                localport = info.LocalEndPoint.Port.ToString();
+                remoteip = info.RemoteEndPoint.Address.ToString();
+                remoteport = info.RemoteEndPoint.Port.ToString();
+                TcpState1 = info.State.ToString();
+                loc = GetLocation(remoteip);
+                mapable = loc.success;
+                if (mapable)
+                {
+                    maplist+=1;
+                    pt=new point(loc.latitude,loc.longitude);
+                    mappoints.Add(pt);
+                }
+                rtb.AppendText(localip + ":" + localport + " ==> " + remoteip + ":" + remoteport + "  " + TcpState1 + " (" + mapable + "," + loc.longitude + "," + loc.latitude + ")" + nl);
+                tcpconnlist += 1;
+                InvalidateAll();
+
+            }
+            //// TCP LISTENERS
+            //// TCP LISTENERS
+            //// TCP LISTENERS
+            string ip2 = "";
+            string port2 = "";
+            rtbTl.AppendText("== TCP Listeners ==" + nl);
+            foreach (IPEndPoint info in endPointsTcp)
+            {
+                ip2 = info.Address.ToString();
+                if (ip2.Contains(":"))
+                { continue; }
+                port2 = info.Port.ToString();
+                rtbTl.AppendText(ip2 + ":" + port2 + nl);
+                tcplist += 1;
+            }
+            //// UDP LISTENERS
+            //// UDP LISTENERS
+            //// UDP LISTENERS
+            string ip3 = "";
+            string port3 = "";
+            rtbUl.AppendText("== UDP Listeners ==" + nl);
+            foreach (IPEndPoint info in endPointsUdp)
+            {
+                ip3 = info.Address.ToString();
+                if (ip3.Contains(":"))
+                { continue; }
+                port3 = info.Port.ToString();
+                rtbUl.AppendText(ip3 + ":" + port3 + nl);
+                udplist += 1;
+            }
+            lstats.Text = "TCP connections = " + tcpconnlist + " IPv4 || UDP listeners = " + udplist + " IPv4 / " + Ustats.UdpListeners.ToString() + " IPv4&&6 || TCP listeners = " + tcplist + " IPv4 / " + Tstats.CurrentConnections.ToString() + " IPv4&&6";
+            InvalidateAll();
+        }
+
+         private void bb_Click(object sender, EventArgs e)
+        {
+            rrtb.Clear();
+            string ip = tip.Text;
+            location tmp;
+           tmp= GetLocation(ip);
+            string strres = "(" + tmp.success + "," + tmp.latitude + "," + tmp.longitude + ")";
+            rrtb.AppendText(strres);
+
+        }
+        public location GetLocation(string strIP)
+        {
+            location temp1 = new location();
+           
+           try
+            {
+               string apikey = "a375f9c927c2ffa58c56e47cb6d9890eaefa74412271fbd72e1f7fcb3fb8b549"; // had to sign up for free account to get this api key
+               string path = "http://api.ipinfodb.com/v3/ip-city/?key=" + apikey + "&ip=" + strIP;
+               WebClient client = new WebClient();
+               string eResult = client.DownloadString(path).ToString();
+               char delimiter= ';';
+               string[] splits = eResult.Split(delimiter);
+               temp1.success = true;
+               temp1.latitude=Convert.ToDouble(splits[8]);
+               temp1.longitude=Convert.ToDouble(splits[9]);
+            }
+            catch
+            {
+                temp1.success = false;
+                return temp1;
+            }
+           if (temp1.latitude == 0 && temp1.longitude == 0) { temp1.success = false; }
+            return temp1;
+        }
+    }
+    public class location
+    {
+        public bool success = false;
+        public double latitude = 0;
+        public double longitude = 0;
+    }
+    public class point
+    {
+        public double latitude = 0;
+        public double longitude = 0;
+        public point(double latitudenum, double longitudenum) { latitude = latitudenum; longitude = longitudenum; }
+    }
+  }
+
+
